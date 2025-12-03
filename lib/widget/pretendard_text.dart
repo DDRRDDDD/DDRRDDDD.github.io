34b0856e76d74204a7507890bd4187d438a88349
DDRRDDDD <youngmin4018@gmail.com>
2025-12-04 02:24:54 +0900

    feat: Typography 열거형 추가 및 PretendardText 클래스 수정
    - Typography 열거형을 추가하여 다양한 텍스트 스타일을 정의함
    - PretendardText 클래스에서 typography 매개변수를 필수로 변경
    - 텍스트 스타일 적용 방식을 개선하여 가독성 향상

--- lib/widget/pretendard_text.dart
+++ lib/widget/pretendard_text.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
-import '../token/app_typography.dart';
-
 class PretendardText extends StatelessWidget {
   const PretendardText({
     super.key,
-    this.typography,
     this.color,
+    required this.typography,
     required this.text,
   }) : textSpan = null;
 
   const PretendardText.rich({
     super.key,
-    this.typography,
     this.color,
+    required this.typography,
     required this.textSpan,
   }) : text = null;
 
-  final AppTypography? typography;
+  final Typography typography;
   final Color? color;
   final String? text;
   final TextSpan? textSpan;
@@ -27,9 +25,45 @@ class PretendardText extends StatelessWidget {
     return Text.rich(
       TextSpan(
         text: text,
-        style: typography?.style(color: color),
+        style: typography.textStyle(color: color),
         children: [?textSpan],
       ),
+    );
+  }
+}
+
+enum Typography {
+  headline1(
+    fontWeight: bold,
+    fontSize: 24,
+    lineHeight: 29,
+  );
+
+  static const String fontFamily = 'Pretendard';
+  static const double letterSpacing = -0.25;
+
+  static const FontVariation medium = FontVariation.weight(500);
+  static const FontVariation semiBold = FontVariation.weight(600);
+  static const FontVariation bold = FontVariation.weight(700);
+
+  final FontVariation fontWeight;
+  final double fontSize;
+  final double lineHeight;
+
+  const Typography({
+    required this.fontWeight,
+    required this.fontSize,
+    required this.lineHeight,
+  });
+
+  TextStyle textStyle({Color? color}) {
+    return TextStyle(
+      color: color,
+      fontFamily: fontFamily,
+      letterSpacing: letterSpacing,
+      fontVariations: [fontWeight],
+      fontSize: fontSize,
+      height: lineHeight / fontSize,
     );
   }
 }
