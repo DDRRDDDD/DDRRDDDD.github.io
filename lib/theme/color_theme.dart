cf463ef006a53df1c8cb77a5b2d219a8070baea2
DDRRDDDD <youngmin4018@gmail.com>
2025-12-04 02:23:55 +0900

    feat: 색상 테마 관리 클래스 추가
    - 색상 테마를 정의하는 ColorTheme 인터페이스 추가
    - ColorThemeExtension 클래스를 통해 테마 확장 기능 구현
    - ColorPalette 열거형을 사용하여 라이트 및 다크 테마 색상 정의

---
+++ lib/theme/color_theme.dart
@@ -0,0 +1,45 @@
+p// ignore_for_file: annotate_overrides
+
+import 'package:flutter/material.dart';
+
+abstract interface class ColorTheme {
+  Color get primary;
+}
+
+class ColorThemeExtension extends ThemeExtension<ColorThemeExtension>
+    implements ColorTheme {
+  const ColorThemeExtension({required this.delegate});
+
+  final ColorTheme delegate;
+
+  @override
+  ColorThemeExtension copyWith({ColorTheme? colorTheme}) {
+    return ColorThemeExtension(delegate: colorTheme ?? delegate);
+  }
+
+  @override
+  ColorThemeExtension lerp(ColorThemeExtension? other, double t) {
+    return this;
+  }
+
+  @override
+  Color get primary {
+    return delegate.primary;
+  }
+}
+
+enum ColorPalette implements ColorTheme {
+  light(
+    primary: Color(0xFF1E88E5),
+  ),
+  dark(
+    primary: Color(0xFF90CAF9),
+  )
+  ;
+
+  final Color primary;
+
+  const ColorPalette({
+    required this.primary,
+  });
+}
