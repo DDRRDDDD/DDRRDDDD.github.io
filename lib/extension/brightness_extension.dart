bd745da7b11ef67df66e63f9a806e15aec727f30
DDRRDDDD <youngmin4018@gmail.com>
2025-12-04 03:34:01 +0900

    feat: BrightnessExtension 추가 및 반전 기능 구현
    - Brightness에 대한 확장을 추가하여
      반전된 밝기를 반환하는 opposite 메서드와
      밝기에 따라 ThemeMode를 반환하는 themeMode 메서드를 구현함.

---
+++ lib/extension/brightness_extension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension BrightnessExtension on Brightness {
+  Brightness get opposite {
+    return this == Brightness.light ? Brightness.dark : Brightness.light;
+  }
+
+  ThemeMode get themeMode {
+    return this == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+  }
+}
