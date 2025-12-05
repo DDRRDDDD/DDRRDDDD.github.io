3c66e987d859c20c65f8141ded8847f322ffd665
DDRRDDDD <youngmin4018@gmail.com>
2025-12-05 18:11:45 +0900

    feat: 폰트 테마 및 확장 추가
    - 새로운 FontTheme 클래스를 추가하여 폰트 관련 설정을 관리합니다.
    - Pretendard 열거형을 통해 다양한 폰트 스타일을 정의합니다.
    - FontThemeExtension 클래스를 추가하여 테마 확장을 지원합니다.
    - MyPortfolioScaffold에 PrimaryAppBar를 추가하여 UI 개선을 도모합니다.
    - MyPortfolioSectionContainer를 추가하여 스크롤 기능을 구현합니다.

---
+++ lib/theme/font_theme.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: annotate_overrides
+
+import 'package:flutter/material.dart';
+
+@immutable
+class FontTheme {
+  final String? fontFamily;
+  final double? letterSpacing;
+  final double fontSize;
+  final double height;
+  final List<FontVariation> fontVariations;
+
+  const FontTheme({
+    this.fontFamily,
+    this.letterSpacing,
+    required this.fontSize,
+    required this.height,
+    required this.fontVariations,
+  });
+}
+
+enum Pretendard implements FontTheme {
+  display(
+    fontWeight: regular,
+    fontSize: 57,
+    height: 1.12,
+  ),
+  headline(
+    fontWeight: regular,
+    fontSize: 32,
+    height: 1.25,
+  ),
+  title(
+    fontWeight: medium,
+    fontSize: 22,
+    height: 1.27,
+  ),
+  body(
+    fontWeight: regular,
+    fontSize: 16,
+    height: 1.50,
+  ),
+  label(
+    fontWeight: medium,
+    fontSize: 12,
+    height: 1.33,
+  )
+  ;
+
+  static const FontVariation regular = FontVariation.weight(400);
+  static const FontVariation medium = FontVariation.weight(500);
+  static const FontVariation semiBold = FontVariation.weight(600);
+  static const FontVariation bold = FontVariation.weight(700);
+
+  final double fontSize;
+  final double height;
+  final FontVariation fontWeight;
+
+  double get letterSpacing => -0.25;
+
+  String get fontFamily => 'Pretendard';
+
+  List<FontVariation> get fontVariations => [fontWeight];
+
+  const Pretendard({
+    required this.fontSize,
+    required this.height,
+    required this.fontWeight,
+  });
+}
+
+class FontThemeExtension extends ThemeExtension<FontThemeExtension>
+    implements FontTheme {
+  const FontThemeExtension({required this.delegate});
+
+  final FontTheme delegate;
+
+  @override
+  FontThemeExtension copyWith({FontTheme? fontTheme}) {
+    return FontThemeExtension(delegate: fontTheme ?? delegate);
+  }
+
+  @override
+  FontThemeExtension lerp(FontThemeExtension? other, double t) {
+    return this;
+  }
+
+  String? get fontFamily => delegate.fontFamily;
+  double? get letterSpacing => delegate.letterSpacing;
+  double get fontSize => delegate.fontSize;
+  double get height => delegate.height;
+  List<FontVariation> get fontVariations => delegate.fontVariations;
+}
--- lib/view/my_portfolio.dart
+++ lib/view/my_portfolio.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
 import '../extension/theme_extension.dart';
+import '../widget/primary_app_bar.dart';
 import '../widget/theme_toggle.dart';
 
 class MyPortfolioScaffold extends StatelessWidget {
@@ -15,6 +17,7 @@ class MyPortfolioScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.colorTheme.background,
+      appBar: const PrimaryAppBar(),
       body: Align(
         alignment: Alignment.topCenter,
         child: FractionallySizedBox(
@@ -23,6 +26,84 @@ class MyPortfolioScaffold extends StatelessWidget {
         ),
       ),
       floatingActionButton: const BrightnessToggle(),
+    );
+  }
+}
+
+class MyPortfolioSectionContainer extends StatefulWidget {
+  const MyPortfolioSectionContainer({
+    super.key,
+    required this.currentIndex,
+    required this.children,
+  });
+
+  final int currentIndex;
+  final List<Widget> children;
+
+  @override
+  State<MyPortfolioSectionContainer> createState() {
+    return _MyPortfolioSectionContainerState();
+  }
+}
+
+class _MyPortfolioSectionContainerState
+    extends State<MyPortfolioSectionContainer> {
+  late final AutoScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AutoScrollController();
+  }
+
+  @override
+  void didUpdateWidget(MyPortfolioSectionContainer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.currentIndex != widget.currentIndex) {
+      _controller.scrollToIndex(
+        widget.currentIndex,
+        preferPosition: .begin,
+        duration: const Duration(milliseconds: 500),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  int get _computeItemCount {
+    return widget.children.length * 2 + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      controller: _controller,
+      slivers: [
+
+        SliverList.builder(
+          itemCount: _computeItemCount,
+          itemBuilder: _buildSectionItem,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSectionItem(_, int index) {
+    if (index.isEven) {
+      return const SizedBox(height: 120);
+    }
+
+    final int itemIndex = index ~/ 2;
+
+    return AutoScrollTag(
+      index: itemIndex,
+      key: ValueKey(itemIndex),
+      controller: _controller,
+      child: widget.children.elementAt(itemIndex),
     );
   }
 }
