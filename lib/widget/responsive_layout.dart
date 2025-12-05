dfc149ba42f693e253c36ddd2884df85ed2541c4
DDRRDDDD <youngmin4018@gmail.com>
2025-12-05 17:02:22 +0900

    rename: responsive_layout.dart 파일로 이름 변경 및 구조 추가
    - ResponsiveLayout 및 ResponsiveScope 클래스를 추가하여
      반응형 레이아웃을 지원합니다.
    - DisplaySize 열거형을 수정하여 화면 크기를 정의합니다.
    - ResponsiveScope를 통해 하위 위젯에 displaySize를
      제공할 수 있도록 구현했습니다.

---
+++ lib/widget/responsive_layout.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveScope(
+      displaySize: DisplaySize.from(context),
+      child: child,
+    );
+  }
+}
+
+class ResponsiveScope extends InheritedWidget {
+  const ResponsiveScope({
+    super.key,
+    required this.displaySize,
+    required super.child,
+  });
+
+  final DisplaySize displaySize;
+
+  static ResponsiveScope of(BuildContext context) {
+    final ResponsiveScope? result = context
+        .dependOnInheritedWidgetOfExactType<ResponsiveScope>();
+
+    if (result != null) {
+      return result;
+    }
+
+    throw FlutterError('No ResponsiveScope found in context');
+  }
+
+  @override
+  bool updateShouldNotify(ResponsiveScope old) {
+    return displaySize != old.displaySize;
+  }
+}
+
+enum DisplaySize {
+  small,
+  medium,
+  large
+  ;
+
+  factory DisplaySize.from(BuildContext context) {
+    final double width = MediaQuery.sizeOf(context).width;
+
+    return switch (width) {
+      < 600 => DisplaySize.small,
+      < 1200 => DisplaySize.medium,
+      _ => DisplaySize.large,
+    };
+  }
+}
