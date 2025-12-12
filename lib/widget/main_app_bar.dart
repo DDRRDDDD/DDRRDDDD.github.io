2dad24f9c6940f548df9f34918de22b02130f3fb
DDRRDDDD <youngmin4018@gmail.com>
2025-12-12 17:29:53 +0900

    feat: MainAppBarDelegate에서 AppBarLeading을 DeveloperBragging으로 변경
    - AppBarLeading 위젯을 DeveloperBragging으로 교체하여
      코드의 일관성을 높임
    - MenuTextButton 위젯을 추가하여 메뉴 항목을 동적으로 생성

--- lib/widget/main_app_bar.dart
+++ lib/widget/main_app_bar.dart
@@ -1,13 +1,8 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:gap/gap.dart';
 
 import '../extension/theme_extension.dart';
-import '../theme/font_theme.dart';
-import 'custom_text.dart';
-import 'main_app_bar_leading.dart';
+import 'developer_bragging.dart';
+import 'menu_text_button.dart';
 
 class MainAppBarDelegate extends SliverPersistentHeaderDelegate {
   const MainAppBarDelegate({
@@ -33,67 +28,35 @@ class MainAppBarDelegate extends SliverPersistentHeaderDelegate {
     return oldDelegate.onMenuTap != onMenuTap;
   }
 
-  int get _computeItemLength {
-    return labels.length * 2 - 1;
-  }
-
   @override
   Widget build(BuildContext context, double shrinkOffset, _) {
-    return ClipRect(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 10.0,
-          sigmaY: 10.0,
-        ),
-        child: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.95,
-            child: Row(
-              children: [
-                AppBarLeading(
-                  onTap: () => onMenuTap?.call(0),
-                  enabled: shrinkOffset < maxExtent,
-                ),
-                const Spacer(),
-                ...List.generate(
-                  _computeItemLength,
-                  (index) => _buildMenuItem(context, index),
-                ),
-              ],
-            ),
+    return Container(
+      alignment: .center,
+      decoration: BoxDecoration(
+        color: context.colorTheme.background,
+        border: Border(
+          bottom: BorderSide(
+            color: context.colorTheme.outline,
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildMenuItem(BuildContext context, int index) {
-    if (index.isOdd) {
-      return const Gap(40);
-    }
-
-    final int itemIndex = index ~/ 2;
-
-    return Animate(
-      effects: [
-        FadeEffect(
-          duration: 500.ms,
-          delay: (itemIndex * 50).ms,
-        ),
-        SlideEffect(
-          begin: const Offset(0, -5),
-          end: Offset.zero,
-          duration: 500.ms,
-          curve: Curves.easeOutQuart,
-          delay: (itemIndex * 50).ms,
-        ),
-      ],
-      child: GestureDetector(
-        onTap: () => onMenuTap?.call(itemIndex + 1),
-        child: CustomHoverText(
-          fontStyle: Pretendard.title,
-          hoverColor: context.colorTheme.primary,
-          text: labels.elementAt(itemIndex),
+      child: FractionallySizedBox(
+        widthFactor: 0.9,
+        child: Row(
+          children: [
+            DeveloperBragging(
+              onTap: () => onMenuTap?.call(0),
+              enabled: shrinkOffset < maxExtent,
+            ),
+            const Spacer(),
+            ...List.generate(
+              labels.length,
+              (index) => MenuTextButton(
+                onTap: () => onMenuTap?.call(index + 1),
+                text: labels.elementAt(index),
+              ),
+            ),
+          ],
         ),
       ),
     );
