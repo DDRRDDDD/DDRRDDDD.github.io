475106021ea364b1071f5057b23a001016050902
DDRRDDDD <youngmin4018@gmail.com>
2025-12-13 01:51:40 +0900

    Save changes before refreshing .gitignore

---
+++ lib/datasource/email_manager.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:envied/envied.dart';
+import 'package:http/http.dart' as http;
+
+import '../extension/let_extension.dart';
+
+part 'email_manager.g.dart';
+
+@Envied(path: '.env')
+class EmailManager {
+  static final EmailManager _instance = EmailManager._internal();
+  static final Uri _baseUri = Uri.https('api.emailjs.com');
+
+  @EnviedField(varName: 'SERVICE_ID', obfuscate: true)
+  static final String _serviceId = _EmailManager._serviceId;
+
+  @EnviedField(varName: 'TEMPLATE_ID', obfuscate: true)
+  static final String _templateId = _EmailManager._templateId;
+
+  @EnviedField(varName: 'PUBLIC_KEY', obfuscate: true)
+  static final String _userId = _EmailManager._userId;
+
+  const EmailManager._internal();
+
+  factory EmailManager() {
+    return _instance;
+  }
+
+  Future<void> sendEmail(EmailPayload payload) async {
+    await _baseUri
+        .replace(
+          path: '/api/v1.0/email/send',
+          queryParameters: {
+            'service_id': _serviceId,
+            'template_id': _templateId,
+            'user_id': _userId,
+            'template_params': payload.toJson(),
+          },
+        )
+        .let(http.get);
+  }
+}
+
+class EmailPayload {
+  final String fromEmail;
+  final String title;
+  final String message;
+
+  const EmailPayload({
+    required this.fromEmail,
+    required this.title,
+    required this.message,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'from_email': fromEmail,
+      'title': title,
+      'message': message,
+    };
+  }
+}
--- lib/view/contact_section.dart
+++ lib/view/contact_section.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../extension/theme_extension.dart';
+import '../widget/section_title.dart';
 
 class ContactSection extends StatelessWidget {
   const ContactSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: context.colorTheme.surface,
-      child: SizedBox(
-        height: 1000,
+    return Container(
+      width: 1000,
+      padding: const EdgeInsets.symmetric(
+        vertical: 150,
+      ),
+      child: Column(
+        mainAxisAlignment: .center,
+        spacing: 30,
+        children: [
+          const SectionTitle(
+            showDivider: false,
+            title: 'Contact',
+            subtitle:
+                '저와 함께 작업하고 싶거나, 더 많은 정보를 원하신다면 언제든지 연락주세요.\n'
+                '새로운 기회를 기다리고 있습니다!',
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 24,
+              ),
+              foregroundColor: Colors.white,
+              iconColor: Colors.white,
+              backgroundColor: context.colorTheme.primary,
+              textStyle: GoogleFonts.notoSansKr(
+                textStyle: context.textTheme.buttonText,
+                height: 1.5,
+              ),
+            ),
+            icon: const Icon(Icons.email),
+            label: const Text('Send Email'),
+          ),
+        ],
       ),
     );
   }
