d03336b7d1c2d9a22d27a2699811a20e8405c896
DDRRDDDD <youngmin4018@gmail.com>
2025-12-12 17:31:48 +0900

    remove: 사용하지 않는 import 문 제거
    - 'package:collection/collection.dart' 임포트를 제거하여
      코드의 불필요한 의존성을 줄임

--- lib/view/my_portfolio.dart
+++ lib/view/my_portfolio.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
-import 'package:collection/collection.dart';
 
 import '../extension/theme_extension.dart';
 import '../route/section_branch.dart';
