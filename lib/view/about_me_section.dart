2563f19d8e441ac42cc567a91e3df75fea225345
DDRRDDDD <youngmin4018@gmail.com>
2025-12-12 17:27:58 +0900

    feat: AboutMeSection 위젯 개선
    - 색상 테마를 적용하여 배경색 변경
    - Padding 및 SizedBox를 사용하여 레이아웃 개선
    - Core Competencies 섹션 제목 추가

--- lib/view/about_me_section.dart
+++ lib/view/about_me_section.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../extension/theme_extension.dart';
+import '../widget/section_title.dart';
+
 class AboutMeSection extends StatelessWidget {
   const AboutMeSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print('asdasd');
-    return Container(
-      color: Colors.blueAccent,
-      height: 800,
-      width: double.infinity,
+
+    return ColoredBox(
+      color: context.colorTheme.surface,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 200,
+        ),
+        child: SizedBox(
+          height: 1000,
+          child: Column(
+            mainAxisAlignment: .center,
+            spacing: 60,
+            children: [
+              const SectionTitle(
+                title: 'Core Competencies',
+                subtitle: '이유 없는 기술은 쓰지 않습니다. '
+                    '오직 문제 해결과 원활한 소통을 위해 깊이 파고듭니다.',
+              ),
+              Wrap(
+                spacing: 24,
+                runSpacing: 24,
+                alignment: .center,
+                children: [],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
