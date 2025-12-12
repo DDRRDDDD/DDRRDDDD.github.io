b5cd36fc7633f5b597ccdb1cff70d354c0a8dc86
DDRRDDDD <youngmin4018@gmail.com>
2025-12-12 17:29:20 +0900

    feat: ContactSection 위젯의 배경색 및 크기 조정
    - 테마 확장을 통해 배경색을 동적으로 설정
    - 위젯 크기를 1000으로 변경하여 레이아웃 개선

--- lib/view/contact_section.dart
+++ lib/view/contact_section.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../extension/theme_extension.dart';
+
 class ContactSection extends StatelessWidget {
   const ContactSection({super.key});
 
   @override
-  Widget build(BuildContext context) {print('asdasd');
-    return Container(
-      color: Colors.red,
-      height: 800,
-      width: double.infinity,
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: context.colorTheme.surface,
+      child: SizedBox(
+        height: 1000,
+      ),
     );
   }
 }
