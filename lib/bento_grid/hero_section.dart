69135f8afd17d5b328a35f6468e41b40e371607d
DDRRDDDD <youngmin4018@gmail.com>
2025-12-21 23:22:12 +0900

    feat: HeroSection 위젯 추가
    HeroSection 위젯을 추가하여 사용자에게 인사와 함께
    주요 메시지를 전달합니다. LabelChip과 GradientTextSpan
    위젯을 활용하여 시각적으로 매력적인 구성 요소를
    제공합니다. 이를 통해 사용자 경험을 향상시키고
    포트폴리오의 가독성을 높입니다.

---
+++ lib/bento_grid/hero_section.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../extension/theme_extension.dart';
+import '../theme/color_theme.dart';
+import '../widget/gradient_text_span.dart';
+import '../widget/label_chip.dart';
+
+class HeroSection extends StatelessWidget {
+  const HeroSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: .center,
+      crossAxisAlignment: .start,
+      spacing: 24,
+      children: [
+        Wrap(
+          spacing: 8,
+          runSpacing: 8,
+          children: [
+            LabelChip(
+              chipColor: context.chipColorTheme.heroPrimary,
+              label: '집요한 문제 해결',
+            ),
+            LabelChip(
+              chipColor: context.chipColorTheme.neutral,
+              label: '소통을 설계하는 아키텍처',
+            ),
+          ],
+        ),
+        Text.rich(
+          TextSpan(
+            style: GoogleFonts.notoSansKr(
+              textStyle: context.textTheme.heroTitle,
+              color: context.colorTheme.textMain,
+            ),
+            children: [
+              TextSpan(
+                text: '안녕하세요,\n',
+              ),
+              GradientTextSpan(
+                style: GoogleFonts.notoSansKr(
+                  textStyle: context.textTheme.heroTitle,
+                ),
+                colors: [
+                  context.colorTheme.primary,
+                  ColorThemeExtension.primaryGradientEnd,
+                ],
+                text: '플러터 개발자\n',
+              ),
+              TextSpan(
+                text: '김용민입니다.',
+              ),
+            ],
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 450,
+            minWidth: 300,
+          ),
+          child: Text(
+            '임시방편은 없습니다. 집요하게 문제의 뿌리를 뽑습니다. \n'
+            '27만 명이 사용하는 오픈소스의 오류를 해결하여 기여한 경험이 저를 증명합니다. '
+            '단순한 기능 구현을 넘어, 코드로 제품의 신뢰를 쌓는 개발자가 되고 싶습니다.',
+            style: GoogleFonts.notoSansKr(
+              textStyle: context.textTheme.bodyLead,
+              color: context.colorTheme.textMain,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
