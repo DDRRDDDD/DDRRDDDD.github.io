8dc7f5531b7386713560ad17b09fe35e3e0ce924
DDRRDDDD <youngmin4018@gmail.com>
2025-12-31 01:18:32 +0900

    feat: 테마 전환 버튼 위젯 추가
    - 테마 전환을 위한 새로운 버튼 위젯을 구현하였습니다.
    - Rive 애니메이션을 사용하여 시각적 효과를 추가하였습니다.
    - GlassContainer를 활용하여 디자인을 개선하였습니다.

---
+++ lib/widget/brightness_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+import '../datasource/rive_file_manager.dart';
+import '../extension/theme_extension.dart';
+import 'glass_container.dart';
+
+class ThemeToggleButton extends StatefulWidget {
+  static const double buttonSize = 56.0;
+
+  const ThemeToggleButton({super.key});
+
+  @override
+  State<ThemeToggleButton> createState() => _ThemeToggleButtonState();
+}
+
+class _ThemeToggleButtonState extends State<ThemeToggleButton> {
+  late RiveWidgetController _controller;
+  late ViewModelInstance _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = RiveWidgetController(RiveFileManager().themeToggle);
+    _viewModel = _controller.dataBind(DataBind.auto());
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _viewModel.color('stroke')?.value = context.colorTheme.textSub;
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassContainer(
+      width: ThemeToggleButton.buttonSize,
+      height: ThemeToggleButton.buttonSize,
+      borderRadius: BorderRadius.circular(
+        ThemeToggleButton.buttonSize / 2,
+      ),
+      child: InkWell(
+        onTap: () {},
+        customBorder: const CircleBorder(),
+        child: SizedBox.square(
+          dimension: 20,
+          child: RiveWidget(
+            controller: _controller,
+            fit: Fit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
