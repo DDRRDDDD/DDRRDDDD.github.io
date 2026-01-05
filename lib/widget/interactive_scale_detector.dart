40875eaf40d9b3e95b35a6d83fe414cba1d6b683
DDRRDDDD <youngmin4018@gmail.com>
2026-01-05 17:06:36 +0900

    feat: InteractiveScaleDetector 위젯 추가
    - 사용자 상호작용을 감지하여 스케일 애니메이션을 적용하는
      위젯을 추가했습니다.
    - onTap 및 onHover 콜백을 통해 사용자 이벤트를 처리합니다.
    - 마우스 커서 상태에 따라 적절한 커서를 표시합니다.

---
+++ lib/widget/interactive_scale_detector.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import '../extension/let_extension.dart';
+import '../extension/widget_states_extension.dart';
+import 'provider.dart';
+
+class InteractiveScaleDetector extends StatefulWidget {
+  const InteractiveScaleDetector({
+    super.key,
+    this.onTap,
+    this.onHover,
+    this.hoverScale = 0.04,
+    required this.child,
+  });
+
+  final VoidCallback? onTap;
+  final VoidCallback? onHover;
+  final double hoverScale;
+  final Widget child;
+
+  @override
+  State<InteractiveScaleDetector> createState() {
+    return _InteractiveScaleDetectorState();
+  }
+}
+
+class _InteractiveScaleDetectorState extends State<InteractiveScaleDetector> {
+  late final WidgetStatesController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WidgetStatesController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  MouseCursor get _cursor {
+    return widget.onTap != null ? SystemMouseCursors.click : MouseCursor.defer;
+  }
+
+  double _resolveScale(WidgetStates states) {
+    double scale = 1.0;
+    scale += states.isHovered ? widget.hoverScale : 0.0;
+    scale -= states.isPressed ? widget.hoverScale : 0.0;
+
+    return scale;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<WidgetStatesController>(
+      notifier: _controller,
+      child: MouseRegion(
+        cursor: _cursor,
+        onEnter: (_) {
+          _controller.hoverOn();
+          widget.onHover?.call();
+        },
+        onExit: (_) {
+          _controller.hoverOff();
+        },
+        child: GestureDetector(
+          onTapUp: (_) {
+            _controller.pressOff();
+            widget.onTap?.call();
+          },
+          onTapCancel: () {
+            _controller.pressOff();
+          },
+          onTapDown: (_) {
+            _controller.pressOn();
+          },
+          child: ValueListenableBuilder(
+            valueListenable: _controller,
+            builder: _buildScalableChild,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildScalableChild(_, WidgetStates states, _) {
+    return AnimatedScale(
+      scale: WidgetStateProperty.resolveWith(_resolveScale).resolve(states),
+      duration: const Duration(milliseconds: 150),
+      curve: Curves.easeOutCubic,
+      child: widget.child,
+    );
+  }
+}
