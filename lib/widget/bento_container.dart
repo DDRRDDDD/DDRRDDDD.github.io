e28b184e7b1ce493ea6e4e2b4c0df4eedad67d2c
DDRRDDDD <youngmin4018@gmail.com>
2026-01-07 18:59:37 +0900

    feat: BentoContainer의 패딩 속성 추가 및 기본 패딩 수정
    - padding 속성을 추가하여 유연한 패딩 설정 가능
    - contentPadding을 cardPadding으로 변경하여 가독성 향상
    - 기본 패딩 값을 수평 20, 수직 16으로 조정

--- lib/widget/bento_container.dart
+++ lib/widget/bento_container.dart
@@ -6,7 +6,11 @@ class BentoContainer extends StatelessWidget {
   static const double bentoWidth = 240;
   static const double bentoHeight = 220;
   static const double bentoGap = 20;
-  static const EdgeInsets contentPadding = EdgeInsets.all(24);
+  static const EdgeInsets cardPadding = EdgeInsets.all(24);
+  static const EdgeInsets contentPadding = EdgeInsets.symmetric(
+    horizontal: 20,
+    vertical: 16,
+  );
   static const BorderRadius borderRadius = BorderRadius.all(defaultRadius);
 
   const BentoContainer({
@@ -14,6 +18,7 @@ class BentoContainer extends StatelessWidget {
     this.width = double.infinity,
     this.height = double.infinity,
     this.color = Colors.transparent,
+    this.padding,
     this.border,
     required this.child,
   });
@@ -21,6 +26,7 @@ class BentoContainer extends StatelessWidget {
   final double width;
   final double height;
   final Color color;
+  final EdgeInsets? padding;
   final Border? border;
   final Widget child;
 
@@ -29,6 +35,7 @@ class BentoContainer extends StatelessWidget {
     return AnimatedContainer(
       width: width,
       height: height,
+      curve: Curves.easeOut,
       duration: const Duration(milliseconds: 150),
       decoration: BoxDecoration(
         color: color,
@@ -36,7 +43,7 @@ class BentoContainer extends StatelessWidget {
         borderRadius: borderRadius,
       ),
       child: Padding(
-        padding: contentPadding,
+        padding: padding ?? cardPadding,
         child: child,
       ),
     );
