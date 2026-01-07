3c170f10a1942e2039c739d96e3465bfa32c60b3
DDRRDDDD <youngmin4018@gmail.com>
2026-01-08 02:51:11 +0900

    feat: 프로젝트 그리드 컴포넌트 추가
    - 프로젝트를 위한 그리드 레이아웃을 구현
    - 애니메이션 효과가 적용된 그리드 배치 추가
    - 다양한 색상의 박스를 포함한 레이아웃 구성

---
+++ lib/view/project_grid.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+
+import '../extension/grid_extension.dart';
+import '../widget/animate_named_grid_placement.dart';
+import '../widget/bento_container.dart';
+
+class ProjectGrid extends StatelessWidget {
+  const ProjectGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.05,
+        vertical: 50,
+      ),
+      child: Center(
+        child: LayoutGrid(
+          areas: '''
+                  main    main   item1     item1  
+                  main    main   sub_main  sub_main
+                  item2   item2  sub_main  sub_main   
+                  ''',
+          columnSizes: BentoContainer.bentoWidth.px * 4,
+          rowSizes: BentoContainer.bentoHeight.px * 3,
+          columnGap: BentoContainer.bentoGap,
+          rowGap: BentoContainer.bentoGap,
+          children: [
+            AnimateNamedGridPlacement(
+              order: 1,
+              areaName: 'main',
+              child: _buildColoredBox(Colors.redAccent),
+            ),
+            AnimateNamedGridPlacement(
+              order: 2,
+              areaName: 'item1',
+              child: _buildColoredBox(Colors.yellowAccent),
+            ),
+            AnimateNamedGridPlacement(
+              order: 3,
+              areaName: 'sub_main',
+              child: _buildColoredBox(Colors.greenAccent),
+            ),
+            AnimateNamedGridPlacement(
+              order: 4,
+              areaName: 'item2',
+              child: _buildColoredBox(Colors.purpleAccent),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColoredBox(Color color) {
+    return BentoContainer(
+      color: color,
+      child: const SizedBox.shrink(),
+    );
+  }
+}
