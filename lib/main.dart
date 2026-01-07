f84c9b7de3c1421b585a39566eec4b60c03c9d01
DDRRDDDD <youngmin4018@gmail.com>
2026-01-07 18:59:28 +0900

    remove: 불필요한 주석 삭제
    - 코드 가독성을 높이기 위해 주석을 제거했습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ class MyPortfolioApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TwoDimensionalScrollView
     return BrightnessProvider(
       builder: (_, brightness) => ResponsiveLayout(
         child: MaterialApp.router(
