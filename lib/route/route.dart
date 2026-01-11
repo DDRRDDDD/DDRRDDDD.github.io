ae8edc23432f67567ea2dae3d8479e080aea523a
DDRRDDDD <youngmin4018@gmail.com>
2026-01-12 02:39:07 +0900

    feat: 입력 다이얼로그 라우트 추가
    - 입력 다이얼로그를 위한 새로운 라우트를 추가하여
      사용자가 입력할 수 있는 UI를 제공함.
    - 루트 내비게이터 키를 설정하여 다이얼로그를
      올바르게 표시할 수 있도록 함.

--- lib/route/route.dart
+++ lib/route/route.dart
@@ -1,12 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../view/bento_grid_container.dart';
 import '../view/bento_grid_scaffold.dart';
 import '../view/main_grid.dart';
 import '../view/project_grid.dart';
+import '../widget/input_dialog.dart';
+import 'dialog_route.dart';
+
+typedef NavigationKey = GlobalKey<NavigatorState>;
+final NavigationKey rootNavigatorKey = NavigationKey(debugLabel: 'root');
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
+  navigatorKey: rootNavigatorKey,
   routes: [
     StatefulShellRoute(
       navigatorContainerBuilder: BentoGridContainer.shell,
@@ -17,6 +24,15 @@ final GoRouter router = GoRouter(
             GoRoute(
               path: '/',
               builder: (_, _) => const MainGrid(),
+              routes: [
+                GoRoute(
+                  path: 'input',
+                  parentNavigatorKey: rootNavigatorKey,
+                  pageBuilder: (_, _) => DialogPage(
+                    child: InputDialog(),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
