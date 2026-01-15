c16a6b7ac406dc4c2ab5c566100b8e3e7888bd0f
DDRRDDDD <youngmin4018@gmail.com>
2026-01-16 00:13:08 +0900

    refactor: GradientTheme의 색상 조정으로 시각적 일관성 개선
    - 그라디언트 색상 조정을 통해 테마의 시각적 일관성을
      높임
    - 색상 변경으로 사용자 경험 향상

--- lib/theme/gradient_theme.dart
+++ lib/theme/gradient_theme.dart
@@ -6,12 +6,12 @@ const lightGradientTheme = GradientThemeExtension._(
   overlay: LinearGradient(
     begin: .topCenter,
     end: .bottomCenter,
-    stops: [0.0, 0.3, 0.65, 1.0],
+    stops: [0.0, 0.5, 0.7, 1.0],
     colors: [
-      Colors.white38,
+      Colors.black45,
       Colors.transparent,
-      Colors.white38,
-      Colors.white60,
+      Colors.white54,
+      Colors.white70,
     ],
   ),
 );
