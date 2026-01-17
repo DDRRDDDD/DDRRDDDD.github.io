6f1e35b02bb3af4f2204873a4cd566c28be7d5c5
DDRRDDDD <youngmin4018@gmail.com>
2026-01-18 03:44:46 +0900

    feat: 슬라이드 시트 페이지에서 MarkdownViewer 제거
    - 불필요한 위젯을 제거하여 코드 간결성 향상
    - UI 구성 요소 최적화를 위한 리팩토링 진행

--- lib/route/side_sheet_page.dart
+++ lib/route/side_sheet_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
 import '../extension/transition_extension.dart';
-import '../widget/markdown_viewer.dart';
-import '../widget/property_item.dart';
 
 class SlideSheetPage<T> extends CustomTransitionPage<T> {
   const SlideSheetPage({
@@ -66,55 +63,4 @@ class SlideSheetPage<T> extends CustomTransitionPage<T> {
       ),
     );
   }
-}
-
-List<Widget> buildSlivers(BuildContext context) {
-  const EdgeInsets contentPadding = EdgeInsets.symmetric(horizontal: 24);
-
-  return [
-    const SliverGap(20),
-    SliverPadding(
-      padding: contentPadding,
-      sliver: SliverList.list(
-        children: [
-          PropertyTitle(
-            icon: Icons.smartphone,
-            title: "기획서 작성하기",
-          ),
-          const Gap(12),
-          PropertyListTile(
-            icon: Icons.person_outline,
-            label: "담당자",
-            value: "김플러터",
-          ),
-          PropertyListTile(
-            icon: Icons.calendar_today_outlined,
-            label: "마감일",
-            value: "2024년 5월 20일",
-          ),
-          PropertyListTile(
-            icon: Icons.label_outline,
-            label: "태그",
-            value: "기획, 디자인",
-          ),
-          PropertyListTile(
-            icon: Icons.check_circle_outline,
-            label: "상태",
-            value: "진행 중",
-          ),
-        ],
-      ),
-    ),
-    SliverPadding(
-      padding: contentPadding + EdgeInsets.symmetric(vertical: 12),
-      sliver: SliverGap(1, color: Colors.grey[300]),
-    ),
-    SliverPadding(
-      padding: contentPadding,
-      sliver: SliverToBoxAdapter(
-        child: MarkdownViewer(),
-      ),
-    ),
-    const SliverGap(20),
-  ];
-}
+}π
