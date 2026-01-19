2cbfceb2824ff048e651dc4ed9156a5ffe0e118d
DDRRDDDD <youngmin4018@gmail.com>
2026-01-19 19:44:54 +0900

    feat: 프로젝트 정보에 역할 및 기간 속성 추가
    프로젝트 정보를 보다 명확하게 전달하기 위해 다음과 같은
    속성을 추가했습니다:
    - 역할(role) 속성 추가
    - 시작일(startAt) 및 종료일(endAt) 속성 추가
    - 설명(description)에서 줄바꿈 추가

--- lib/constraint/project.dart
+++ lib/constraint/project.dart
@@ -6,9 +6,12 @@ enum Project {
     type: ProjectType.main,
     title: '원아워',
     subTitle: '이웃과 소소한 모임부터 대화까지',
-    description: '시간 남을 때 근처 사람과 가볍게 만날 수 있는 번개 모임 및 이웃 연결 대화 서비스입니다.',
+    description: '시간 남을 때 근처 사람과 가볍게 만날 수 있는 \n번개 모임 및 이웃 연결 대화 서비스',
     labels: ['Flutter', 'Firebase', 'Riverpod'],
     bannerAssetPath: 'assets/image/one-hour.jpg',
+    role: '클라이언트 개발',
+    startAt: '2024-12',
+    endAt: '2025-02',
   ),
   myTurn(
     primaryIcon: Icons.flag,
@@ -18,13 +21,22 @@ enum Project {
     description: '\'원아워\'의 피벗 프로젝트,\n모임의 규모를 보드게임으로 집중한 애플리케이션',
     labels: ['Flutter', 'Supabase', 'Riverpod', 'GoRouter'],
     bannerAssetPath: 'assets/image/my-turn.jpg',
+    role: '앱 개발 리드 및 UI/UX 디자인',
+    startAt: '2025-07',
+    endAt: '2025-12',
   ),
   myPortfolio(
     primaryIcon: Icons.code,
     type: ProjectType.sub,
     subTitle: '플러터 포트폴리오 웹사이트',
     labels: ['Flutter', 'GoRouter', 'Forge2D', 'Rive'],
+    role: '1인 전담 개발 (기획·디자인·구현)',
+    startAt: '2025-12',
   );
+
+  /// 슬라이드 시트 테마/스타일 정리 -> 제미나이 참고
+  /// 프로젝트 아이콘 -> 이모지로 변경
+  /// **마크다운 작성**
 
   final IconData primaryIcon;
   final ProjectType type;
@@ -33,6 +45,9 @@ enum Project {
   final String? description;
   final List<String> labels;
   final String? bannerAssetPath;
+  final String role;
+  final String startAt;
+  final String? endAt;
 
   const Project({
     required this.primaryIcon,
@@ -42,7 +57,14 @@ enum Project {
     this.description,
     required this.labels,
     this.bannerAssetPath,
+    required this.role,
+    required this.startAt,
+    this.endAt,
   });
+
+  bool get isCompleted {
+    return endAt != null;
+  }
 }
 
 enum ProjectType {
