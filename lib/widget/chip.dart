b2112386e3984d48895a0ce0a1848f3e0b663123
DDRRDDDD <youngmin4018@gmail.com>
2026-01-21 17:29:44 +0900

    feat: LinkedTagChip 컴포넌트 추가 및 TagChip 수정
    - TagChip에서 아이콘을 제거하고 텍스트만 표시하도록 변경
    - LinkedTagChip 컴포넌트를 추가하여 링크 기능 구현
    - ScaleDetector를 사용하여 호버 효과 추가

--- lib/widget/chip.dart
+++ lib/widget/chip.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../constraint/project.dart';
+import '../extension/let_extension.dart';
 import '../extension/theme_extension.dart';
+import '../extension/widget_states_extension.dart';
 import '../theme/chip_theme.dart';
+import 'scale_detector.dart';
 
 class LabelChip extends StatelessWidget {
   const LabelChip({
@@ -95,18 +100,10 @@ class IconLabelChip extends StatelessWidget {
 }
 
 class TagChip extends StatelessWidget {
-  const TagChip({
-    super.key,
-    this.icon,
-    required this.tag,
-  });
+  const TagChip({super.key, required this.tag});
 
-  const TagChip.text(
-    this.tag, {
-    super.key,
-  }) : icon = null;
+  const TagChip.text(this.tag, {super.key});
 
-  final IconData? icon;
   final String tag;
 
   @override
@@ -126,24 +123,83 @@ class TagChip extends StatelessWidget {
           horizontal: 10,
           vertical: 5,
         ),
-        child: Row(
-          spacing: 2,
-          mainAxisSize: .min,
-          children: [
-            if (icon != null)
-              FaIcon(
-                icon,
-                size: context.textTheme.labelMedium.fontSize,
-                color: context.colorTheme.textSub,
-              ),
-            Text(
-              tag,
-              style: context.textTheme.labelMedium.copyWith(
-                color: context.colorTheme.textSub,
-                height: 1.5,
-              ),
+        child: Text(
+          tag,
+          style: context.textTheme.labelMedium.copyWith(
+            color: context.colorTheme.textSub,
+            height: 1.5,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LinkedTagChip extends StatelessWidget {
+  const LinkedTagChip({
+    super.key,
+    required this.icon,
+    required this.tag,
+    this.url,
+  });
+
+  factory LinkedTagChip.item(LinkItem item) {
+    return LinkedTagChip(
+      icon: item.icon,
+      tag: item.label,
+      url: Uri.parse(item.url),
+    );
+  }
+
+  final IconData icon;
+  final String tag;
+  final Uri? url;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleDetector(
+      onPressUp: () => url?.let(launchUrl),
+      child: Builder(
+        builder: (context) => AnimatedContainer(
+          duration: const Duration(milliseconds: 150),
+          curve: Curves.easeOutCubic,
+          decoration: BoxDecoration(
+            color: ScaleDetector.of(context).value.isHovered
+                ? context.colorTheme.background
+                : context.colorTheme.glassOverlay,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(6),
             ),
-          ],
+            border: Border.all(
+              color: ScaleDetector.of(context).value.isHovered
+                  ? context.colorTheme.secondary
+                  : context.colorTheme.outline,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 5,
+            ),
+            child: Row(
+              spacing: 6,
+              mainAxisSize: .min,
+              children: [
+                FaIcon(
+                  icon,
+                  size: 18,
+                  color: context.colorTheme.textSub,
+                ),
+                Text(
+                  tag,
+                  style: context.textTheme.labelMedium.copyWith(
+                    color: context.colorTheme.textSub,
+                    height: 1.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
