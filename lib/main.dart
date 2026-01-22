d6258abddbac1c8382b11ebd13043f318c3826a8
DDRRDDDD <youngmin4018@gmail.com>
2026-01-23 01:23:24 +0900

    feat: AssetFinder 초기화 기능 추가
    - AssetFinder를 초기화하여 자산 관리 효율성을 개선
    - 포트폴리오 수정 시 파일 추가/삭제로 유동적인 자산 관리 가능
    - 기존 코드에서 자산 추가 시 코드 수정 필요성 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+import 'datasource/asset_finder.dart';
 import 'datasource/rive_file_manager.dart';
 import 'datasource/svg_manager.dart';
 import 'extension/brightness_extension.dart';
@@ -17,10 +18,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
 
+  await AssetFinder.init();
   await Future.wait([
     RiveFileManager.init(),
     SvgManager.init(SkillOptions.allAssetPaths),
   ]);
+
   runApp(const MyPortfolioApp());
 }
 
@@ -53,3 +56,13 @@ class MyPortfolioApp extends StatelessWidget {
 }
 
 // https://ddrrdddd.github.io/
+/// TODO
+/// 0. rive_file_manager 리펙토링
+
+/// 1. font 서브셋 빌드러너 생성
+/// -> font 파일이 너무 커서 경령화하여 웹 초기 로딩을 빠르게 가져가기 위함
+
+/// 2. AssetFinder를 사용해서 asset을 사용하는 클래스 리펙토링
+/// -> 포트폴리오의 수정이 필요할 시 파일의 추가/삭제만으로 포토폴리오를 핸들링 하기 위함
+/// -> 기존은 asset 파일을 추가하면 코드를 추가해야 함 (비효율)
+/// -> 마크다운, Skill Ball 추가를 유동적으로 가져갈 수 있음
