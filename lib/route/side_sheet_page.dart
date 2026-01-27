54b489ce2ce1bcc2ba160da319cb4d66b07a703c
DDRRDDDD <youngmin4018@gmail.com>
2026-01-27 22:50:41 +0900

    refactor: side_sheet_page에서 너비 비율 조정
    - 사이드 시트의 너비 비율을 0.5에서 0.48로 변경하여
      UI의 균형을 개선합니다.

--- lib/route/side_sheet_page.dart
+++ lib/route/side_sheet_page.dart
@@ -49,7 +49,7 @@ class SlideSheetPage<T> extends CustomTransitionPage<T> {
         child: Align(
           alignment: AlignmentDirectional.centerEnd,
           child: SizedBox(
-            width: (MediaQuery.widthOf(context) * 0.5).clamp(0, 800),
+            width: (MediaQuery.widthOf(context) * 0.48).clamp(0, 800),
             child: Material(
               color: Colors.transparent,
               clipBehavior: Clip.hardEdge,
