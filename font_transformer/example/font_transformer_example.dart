edb24a55f77b99ce20f55cb46c0a94feb4593ace
DDRRDDDD <youngmin4018@gmail.com>
2026-02-07 18:20:01 +0900

    chore: 프로젝트 설정 파일 추가 및 수정
    - pubspec.yaml 파일을 새로 생성하고 기본 설정 추가
    - .gitignore 파일 생성하여 Dart 관련 파일 무시 설정
    - analysis_options.yaml 파일 추가하여 linter 및 formatter 규칙 설정
    - font_transformer.dart 및 font_transformer_test.dart 파일 추가하여
      기본 기능 구현 및 테스트 추가

--- font_transformer/example/font_transformer_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:font_transformer/font_transformer.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- font_transformer/test/font_transformer_test.dart
+++ font_transformer/test/font_transformer_test.dart
@@ -2,15 +2,7 @@ import 'package:font_transformer/font_transformer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('calculate', () {
+    expect(calculate(), 42);
   });
 }
