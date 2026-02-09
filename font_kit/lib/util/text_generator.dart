c2ddcc31c5e984970ddefc838002c4db194747ea
DDRRDDDD <youngmin4018@gmail.com>
2026-02-09 22:59:25 +0900

    feat: TextGenerator 클래스 및 하위 클래스 추가
    - 텍스트 생성을 위한 추상 클래스 TextGenerator를 구현
    - PeriodText, TeamSummaryText, ContributionText 클래스 추가
    - 각 클래스에서 generate() 메서드를 통해 텍스트 생성 로직 정의

---
+++ font_kit/lib/util/text_generator.dart
@@ -0,0 +1,53 @@
+import '../glyph_source.dart';
+
+/// 함수형 정의를 위한 클래스
+abstract class TextGenerator implements GlyphSource {
+  const TextGenerator();
+
+  String generate();
+
+  @override
+  String get glyphs {
+    return generate();
+  }
+
+  @override
+  String toString() {
+    return generate();
+  }
+}
+
+class PeriodText extends TextGenerator {
+  final String startAt;
+  final String? endAt;
+
+  const PeriodText([this.startAt = '', this.endAt]);
+
+  @override
+  String generate() {
+    return '$startAt ~ ${endAt ?? '진행 중'}';
+  }
+}
+
+class TeamSummaryText extends TextGenerator {
+  final int count;
+  final String team;
+
+  const TeamSummaryText([this.count = 0, this.team = '']);
+
+  @override
+  String generate() {
+    return '총 $count명 ($team)';
+  }
+}
+
+class ContributionText extends TextGenerator {
+  final int percent;
+
+  const ContributionText([this.percent = 0]);
+
+  @override
+  String generate() {
+    return '기여도 $percent%';
+  }
+}
