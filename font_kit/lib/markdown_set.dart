e19d4d9b174ae690acde8e4259ad5b8bdda96fcb
DDRRDDDD <youngmin4018@gmail.com>
2026-02-09 22:52:37 +0900

    feat: MarkdownSet 클래스 추가 및 glyphs 접근자 구현
    - MarkdownSet 클래스를 추가하여 GlyphSource 인터페이스를 구현함.
    - 지정된 디렉토리에서 Markdown 파일을 읽어와 glyphs를 반환함.
    - 디렉토리 존재 여부 및 파일 읽기 실패 시 오류 메시지를 출력함.

---
+++ font_kit/lib/markdown_set.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+import 'glyph_source.dart';
+
+class MarkdownSet implements GlyphSource {
+  static const String _directoryPath = '../assets/md/';
+
+  const MarkdownSet();
+
+  @override
+  String get glyphs {
+    final directory = Directory(_directoryPath);
+
+    if (!directory.existsSync()) {
+      stderr.writeln('⚠️ 디렉토리를 찾을 수 없습니다: ${directory.absolute.path}');
+      return '';
+    }
+
+    return directory
+        .listSync(recursive: true)
+        .whereType<File>()
+        .where(_isMarkdownExtension)
+        .fold(StringBuffer(), _combineCharacters)
+        .toString();
+  }
+
+  bool _isMarkdownExtension(File file) {
+    return path.extension(file.path).toLowerCase() == '.md';
+  }
+
+  StringBuffer _combineCharacters(StringBuffer buffer, File file) {
+    try {
+      final String fileName = path.basenameWithoutExtension(file.path);
+      final String content = file.readAsStringSync();
+
+      buffer.write(fileName);
+      buffer.write(content);
+    } catch (e) {
+      stderr.writeln('❌ 읽기 실패: ${file.path}');
+      rethrow;
+    }
+
+    return buffer;
+  }
+}
