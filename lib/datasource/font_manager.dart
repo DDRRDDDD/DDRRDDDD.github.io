f0c40d799209494aa7ff1c4db795b19982c1d028
DDRRDDDD <youngmin4018@gmail.com>
2026-02-14 00:43:59 +0900

    feat: 여러 폰트 로더 생성 기능 추가
    - NotoSans, NotoSansEmoji, JetBrainsMono 폰트 로더를
      비동기로 생성하도록 변경
    - 코드 구조를 개선하여 폰트 로딩을 효율적으로 처리

--- lib/datasource/font_manager.dart
+++ lib/datasource/font_manager.dart
@@ -2,12 +2,31 @@ import 'package:flutter/services.dart';
 
 class FontManager {
   static const String notoSansFamily = 'NotoSans';
-
-  static const String _fontAssetPath = 'assets/fonts/NotoSansKR-VariableFont_wght.ttf';
+  static const String notoSansEmojiFamily = 'NotoSansEmoji';
+  static const String jetBrainsMonoFamily = 'JetBrainsMono';
 
   static Future<void> load() async {
-    final FontLoader fontLoader = FontLoader(notoSansFamily);
-    fontLoader.addFont(rootBundle.load(_fontAssetPath));
-    await fontLoader.load();
+    Future.wait([
+      _createFontLoader(
+        fontFamily: notoSansFamily,
+        assetPath: 'assets/fonts/NotoSansKR-VariableFont_wght.ttf',
+      ),
+      _createFontLoader(
+        fontFamily: notoSansEmojiFamily,
+        assetPath: 'assets/fonts/NotoColorEmoji-Regular.ttf',
+      ),
+      _createFontLoader(
+        fontFamily: jetBrainsMonoFamily,
+        assetPath: 'assets/fonts/JetBrainsMono-Medium.ttf',
+      ),
+    ]);
+  }
+
+  static Future<FontLoader> _createFontLoader({
+    required String fontFamily,
+    required String assetPath,
+  }) async {
+    final FontLoader fontLoader = FontLoader(fontFamily);
+    return fontLoader..addFont(rootBundle.load(assetPath));
   }
 }
